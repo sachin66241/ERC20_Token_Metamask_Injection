6e82cfbb19b32f7950f9f8bf5d42bba0f4ea982e
SACHIN <sachhin6624@gmail.com>
2019-12-04 16:59:43 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,6 @@
+// import Web3 from 'web3';
+ import { sam } from 'module.js';
+
+// const web3 = new Web3('ws://localhost:8543');
+// console.log(web3);
+console.log(sam)
---
+++ module.js
@@ -0,0 +1,3 @@
+export function sam(){
+    return "heyyyy";
+}
